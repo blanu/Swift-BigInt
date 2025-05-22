63a1726e9749b1a08a6d9e76d9afa1458ce9e0d8
Dr. Brandon Wiley <brandon@operatorfoundation.org>
2025-05-21 22:49:44 -0500

    Updated swift tools version

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.9
+// swift-tools-version:6.0
 import PackageDescription
 
 let package = Package(
@@ -32,5 +32,5 @@ let package = Package(
             ],
             path: "Tests"),
     ],
-    swiftLanguageVersions: [.v5]
+    swiftLanguageModes: [.v6]
 )
