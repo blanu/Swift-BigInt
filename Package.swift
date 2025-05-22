48b406be8cf79a727d36c2e05e83072bf3b6c024
Dr. Brandon Wiley <brandon@operatorfoundation.org>
2025-05-21 22:54:10 -0500

    Rolled back breaking swift tools version update

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:6.0
+// swift-tools-version:5.9
 import PackageDescription
 
 let package = Package(
@@ -32,5 +32,5 @@ let package = Package(
             ],
             path: "Tests"),
     ],
-    swiftLanguageModes: [.v6]
+    swiftLanguageModes: [.v5]
 )
